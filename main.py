5a67d093c25455505abd65f3c5ed98989e1120cb
NKRI <nkri.dev@gmail.com>
2025-04-03 23:06:06 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Libs
+import fitz
+import os
+
+# Dossier ou sont stocké les PDF
+folder_path = input("Entrer le chemin absolu oû se trouvent vos PDF : ")
+
+# Fichier qui se sont correctement convertie
+file_convert = []
+
+# Récupération de tout les fichiers dans le dossier
+for file in os.listdir(folder_path):
+    if file.lower().endswith(".pdf"):
+        pdf_path = os.path.join(folder_path, file)
+        doc = fitz.open(pdf_path)
+
+        for i, page in enumerate(doc):
+            pix = page.get_pixmap()
+            img_path = os.path.join(folder_path, f"{file[:-4]}_page_{i + 1}.png")
+            pix.save(img_path)
+            file_convert.append(file)
+
+# Succès
+print("=======================================")
+print("Fichier(s) convertie: ")
+for file in file_convert:
+    print(file)
+print("=======================================")
